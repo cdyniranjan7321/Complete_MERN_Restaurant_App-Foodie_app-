f387c63f94c64abdb23c6d231c5f8351d35ad25f
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-06-29 09:40:05 +0545

    first commit

---
+++ foodi-client/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%'>
+        <div className='py-24'>Banner</div>
+    </div>
+  )
+}
+
+export default Banner
