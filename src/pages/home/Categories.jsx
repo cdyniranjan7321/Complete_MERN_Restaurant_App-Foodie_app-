d57c8f0c1b5e43dec77fd1d1558211b473c93d80
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-07-01 09:53:41 +0545

    home

--- src/pages/home/Categories.jsx
+++ src/pages/home/Categories.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Categories = () => {
+  
   return (
     <div className='section-container py- 16'>
         <div className='text-center'>
-          
+
             <p className='subtitle'>Customer Favorites</p>
             <h2 className='title'>Popular Catagories</h2>
         </div>
