795f232f3b1e94e90f712c63e045e0baf833495d
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-07-01 09:41:54 +0545

    change in pages

--- src/pages/home/Categories.jsx
+++ src/pages/home/Categories.jsx
@@ -6,6 +6,7 @@ const Categories = () => {
         <div className='text-center'>
             <p className='subtitle'>Customer Favorites</p>
             <h2 className='title'>Popular Catagories</h2>
+            
         </div>
     </div>
   )
