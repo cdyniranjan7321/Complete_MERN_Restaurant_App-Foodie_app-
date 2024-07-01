5f4ae327f479f9bbab7ae37f39bf0b5b5a940a8d
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-07-01 09:46:24 +0545

    change inhome.jsx

--- src/pages/home/Categories.jsx
+++ src/pages/home/Categories.jsx
@@ -4,9 +4,9 @@ const Categories = () => {
   return (
     <div className='section-container py- 16'>
         <div className='text-center'>
+          
             <p className='subtitle'>Customer Favorites</p>
             <h2 className='title'>Popular Catagories</h2>
-            
         </div>
     </div>
   )
